9725082ff3032e84da541cca94fbd77b8cfbe98b
mikewood <mike.wood@secretescapes.com>
2023-08-11 13:26:01 +0100

    Updates

--- src/types/type-aliases.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// type aliases are used to provide names to type literals
-const myFunc = function (a, b) {
-    return a + b;
-};
-const ten = myFunc(5, 5); // call the function
-// ==========================================================
